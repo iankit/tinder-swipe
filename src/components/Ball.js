d259f23366636d3562f6c59193a90d2f1da6b693
Ankit <iankit17@gmail.com>
2017-07-06 14:55:36 +0530

    left and right swipe

--- src/components/Ball.js
+++ src/components/Ball.js
@@ -3,13 +3,13 @@ import { View, StyleSheet, Animated } from 'react-native';
 
 class Ball extends Component {
   componentWillMount() {
-    this.position = new Animated.ValueXY(0,0);
+    this.position = new Animated.ValueXY(0, 0);
     Animated.spring(this.position, {
-      toValue: { x: 200, y: 500}
+      toValue: { x: 200, y: 500 }
     }).start();
   }
   render() {
-    return(
+    return (
       <Animated.View style={this.position.getLayout()}>
         <View style={styles.ball} />
       </Animated.View>
---
+++ src/components/Deck.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import { Animated, View, PanResponder, Dimensions } from 'react-native';
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
+const SWIPE_THRASHOLD = 0.3 * SCREEN_WIDTH;
+
+class Deck extends Component {
+  constructor() {
+    super();
+    const position = new Animated.ValueXY();
+    const panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: (event, gesture) => {
+        position.setValue({ x: gesture.dx, y: gesture.dy });
+      },
+      onPanResponderRelease: (event, gesture) => {
+        if (gesture.dx > SWIPE_THRASHOLD) {
+          console.log("swipe right");
+        } else if (gesture.dx < -SWIPE_THRASHOLD) {
+          console.log("swipe left");
+        } else {
+          this.resetPosition();
+        }
+      }
+    });
+    this.state = { panResponder, position };
+  }
+  getCardStyle() {
+    const { position } = this.state;
+    const rotate = position.x.interpolate({
+      inputRange: [-SCREEN_WIDTH * 1.5, 0, SCREEN_WIDTH * 1.5],
+      outputRange: ['-120deg', '0deg', '120deg']
+    });
+    return {
+      ...position.getLayout(),
+      transform: [{ rotate }]
+    };
+  }
+  resetPosition() {
+    Animated.spring(this.state.position, {
+      toValue: { x: 0, y: 0 }
+    }).start();
+  }
+  renderCards() {
+    return this.props.data.map((item, index) => {
+      if (index === 0) {
+        return (
+          <Animated.View
+            key={item.id}
+            style={this.getCardStyle()}
+            {...this.state.panResponder.panHandlers}
+          >
+            {this.props.renderCard(item)}
+          </Animated.View>
+        );
+      }
+      return this.props.renderCard(item);
+    });
+  }
+  render() {
+    return (
+      <View>
+        {this.renderCards()}
+      </View>
+    );
+  }
+}
+
+export default Deck;
